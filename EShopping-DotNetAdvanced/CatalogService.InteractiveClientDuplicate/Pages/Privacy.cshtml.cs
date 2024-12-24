e5d63973dfe1e5586a49e685d32cbed5591de9f1
Weam_Mahmoud <Weam_Mahmoud@EPPLKRAW06D8>
2024-12-24 02:16:58 +0200

    added editor.config

--- EShopping-DotNetAdvanced/CatalogService.InteractiveClientDuplicate/Pages/Privacy.cshtml.cs
+++ EShopping-DotNetAdvanced/CatalogService.InteractiveClientDuplicate/Pages/Privacy.cshtml.cs
@@ -1,5 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
-
+﻿using System.Collections;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 namespace CatalogService.Interactive.Pages
 {
     public class PrivacyModel : PageModel
